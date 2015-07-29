6e8b7ed6f310b0c355a6ba1492b6f3f5def8b728
Chris Hilleren <chilleren@gmail.com>
2015-07-28 20:06:44 -0700

    Add mongoose, simplify config

--- config.js
+++ config.js
@@ -2,4 +2,5 @@
 
 module.exports = function (app) {
   app.set("PORT", 3000);
+  app.set("DB_URL", "mongodb://localhost:27017/todo");
 }
--- server.js
+++ server.js
@@ -1,15 +1,31 @@
 "use strict";
 
 var express = require("express");
+var mongoose = require("mongoose");
+
+var config = require("./config");
+var routes = require("./api/routes");
 
 var app = express();
-
-var config = require("./config/_config");
 
 config(app);
 app.set("ROOT_DIR", __dirname);
 
-var port = app.get("PORT");
-var server = app.listen(port, function () {
-  console.log("Listening on port " + port + ".");
+mongoose.connect(app.get("DB_URL"));
+
+var db = mongoose.connection;
+db.on("error", function (err) {
+  console.error(err);
+  throw err;
+});
+
+db.once("open", function () {
+  console.log("Database connection established.");
+
+  routes(app);
+
+  var port = app.get("PORT");
+  var server = app.listen(port, function () {
+    console.log("Listening on port " + port + ".");
+  });
 });
