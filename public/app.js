72bf72226b6460c8d53710a27e35c06a932092e4
Chris Hilleren <chilleren@gmail.com>
2015-07-30 20:11:50 -0700

    add todo creation, updating

--- public/app.js
+++ public/app.js
@@ -1,15 +1,35 @@
 var app = angular.module("todo", []);
 
+app.factory('todos', [function () {
+  return {
+    todos: [
+      {title: "todo 1", isDone: false},
+      {title: "todo 2", isDone: false},
+      {title: "todo 3", isDone: true},
+      {title: "todo 4", isDone: false}
+    ]
+  }
+}]);
+
 app.controller("MainCtrl", [
   "$scope",
-  function ($scope) {
+  "todos",
+  function ($scope, todos) {
     $scope.test = "hello world";
 
-    $scope.todos = [
-      "todo 1",
-      "todo 2",
-      "todo 3",
-      "todo 4"
-    ];
+    $scope.todos = todos.todos;
+
+    $scope.addTodo = function () {
+      if (!$scope.title || $scope.title === "") {
+        return;
+      }
+      $scope.todos.push({title: $scope.title, isDone: false});
+      $scope.title = "";
+    }
+
+    // $scope.completeTodo = function (todo) {
+    //   console.log("heyo")
+    //   todo.isDone = !todo.isDone;
+    // }
   }
 ]);
